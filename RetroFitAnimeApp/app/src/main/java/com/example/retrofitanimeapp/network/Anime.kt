2f20614f342996aa15940eee91dc30a040d0ef24
manindrarana <ranamanindra9@gmail.com>
2025-05-08 13:50:53 +0100

    **

--- RetroFitAnimeApp/app/src/main/java/com/example/retrofitanimeapp/network/Anime.kt
+++ RetroFitAnimeApp/app/src/main/java/com/example/retrofitanimeapp/network/Anime.kt
@@ -4,7 +4,5 @@ data class Anime(
     val title: String,
     val image_url: String?,
     val description: String?,
-    val studio: String?,
     val genres: List<String>?,
-    val start_date: String?
 )
